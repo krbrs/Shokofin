5d6f32ee79e1d2bb2a4c4c7f5b719641a38a8542
Mikal Stordal <mikalstordal@gmail.com>
2025-04-27 01:24:26 +0200

    chore: remove extra whitespace

--- Shokofin/Extensions/StringExtensions.cs
+++ Shokofin/Extensions/StringExtensions.cs
@@ -11,22 +11,22 @@ namespace Shokofin.Extensions;
 
 public static partial class StringExtensions {
     public static string Replace(this string input, Regex regex, string replacement, int count, int startAt)
-        =>  regex.Replace(input, replacement, count, startAt);
+        => regex.Replace(input, replacement, count, startAt);
 
     public static string Replace(this string input, Regex regex, MatchEvaluator evaluator, int count, int startAt)
-        =>  regex.Replace(input, evaluator, count, startAt);
+        => regex.Replace(input, evaluator, count, startAt);
 
     public static string Replace(this string input, Regex regex, MatchEvaluator evaluator, int count)
-        =>  regex.Replace(input, evaluator, count);
+        => regex.Replace(input, evaluator, count);
 
     public static string Replace(this string input, Regex regex, MatchEvaluator evaluator)
-        =>  regex.Replace(input, evaluator);
+        => regex.Replace(input, evaluator);
 
     public static string Replace(this string input, Regex regex, string replacement)
-        =>  regex.Replace(input, replacement);
+        => regex.Replace(input, replacement);
 
     public static string Replace(this string input, Regex regex, string replacement, int count)
-        =>  regex.Replace(input, replacement, count);
+        => regex.Replace(input, replacement, count);
 
     public static void Deconstruct(this IList<string> list, out string first) {
         first = list.Count > 0 ? list[0] : string.Empty;
