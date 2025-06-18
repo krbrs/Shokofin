39d33fb5b5643daf865f503e9186ba400a8a8f06
Mikal S. <7761729+revam@users.noreply.github.com>
2025-06-18 07:00:08 +0200

    fix: remove unnecessary $ in template escape

--- Shokofin/API/ShokoApiClient.cs
+++ Shokofin/API/ShokoApiClient.cs
@@ -502,7 +502,7 @@ public class ShokoApiClient : IDisposable {
     #region TMDB Episode
 
     public Task<TmdbEpisode?> GetTmdbEpisode(string episodeId, bool useDefaultOrdering = false)
-        => GetOrNull<TmdbEpisode>($"/api/v3/TMDB/Episode/{episodeId}?include=Titles,Overviews,Cast,Crew,FileCrossReferences${(useDefaultOrdering ? "&alternateOrderingID=default" : "")}");
+        => GetOrNull<TmdbEpisode>($"/api/v3/TMDB/Episode/{episodeId}?include=Titles,Overviews,Cast,Crew,FileCrossReferences{(useDefaultOrdering ? "&alternateOrderingID=default" : "")}");
 
     public async Task<IReadOnlyList<TmdbEpisode>> GetTmdbEpisodesInTmdbSeason(string seasonId)
         => (await GetOrNull<ListResult<TmdbEpisode>>($"/api/v3/TMDB/Season/{seasonId}/Episode?pageSize=0&include=Titles,Overviews,Cast,Crew,FileCrossReferences").ConfigureAwait(false))?.List ?? [];
